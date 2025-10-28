a877a11731af93622958ddad395ec30e07a218c1
tooez4deepak <126541646+tooez4deepak@users.noreply.github.com>
2025-10-28 13:47:15 +0530

    Update 3Q.sql

--- scripts/business problems/3Q.sql
+++ scripts/business problems/3Q.sql
@@ -4,6 +4,6 @@ SELECT DISTINCT
 	Category,
 	SUM(discountedSellingPrice * availableQuantity) AS Totalrevenue
 FROM gold.zepto
--- Calculate total revenue per category by summing price × quantity
+-- Calculate total revenue per category by summing price Ã— quantity
 GROUP BY Category
 ORDER BY Totalrevenue; -- Sort categories from lowest to highest total revenue
